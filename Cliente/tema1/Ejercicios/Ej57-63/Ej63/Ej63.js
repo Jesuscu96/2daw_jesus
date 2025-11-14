c863f4a4d6200bdaf1686017db19354dc31eddee
solvam <alumno@solvam.local>
2025-11-14 13:19:47 +0100

    ej 63 terminar

--- Cliente/tema1/Ejercicios/Ej57-63/Ej63/Ej63.js
+++ Cliente/tema1/Ejercicios/Ej57-63/Ej63/Ej63.js
@@ -43,9 +43,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 puntosPorPartido: 4.0
             }
         ];
-        const newFrom = () => {
-            0
-        }   
+        // const newFrom = () => {
+        //     0
+        // }   
         const cleanBody = () => {
             miBody.textContent = "";
         }
@@ -56,7 +56,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         //console.log(jugadoresBulls);
-        let miBody = document.body;
+        //let miBody = document.body;
+        let container = document.createElement("div");
+        container.className = "container";
         const createPlayers = (jugadoresBulls) =>{
             for (let i = 0; i < jugadoresBulls.length; i++) {
                 let divPlayerCard = document.createElement("div");
@@ -65,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 pId.classList.add("pId");
                 pId.textContent = jugadoresBulls[i].id
                 divPlayerCard.appendChild(pId)
-                miBody.appendChild(divPlayerCard);
+                container.appendChild(divPlayerCard);
 
                 let imgPlayer = document.createElement("img");
                 imgPlayer.classList.add("player-img");
@@ -105,11 +107,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 pPoint.textContent += jugadoresBulls[i].puntosPorPartido;
                 divPlayerInfo.appendChild(pPoint);
 
-                let button = document.createElement("button");
-                button.classList.add("but");
-                button.textContent = "Remove";
-                divPlayerInfo.appendChild(button);
-
+                let rmBtn = document.createElement("button");
+                rmBtn.classList.add("but");
+                rmBtn.textContent = "Remove";
+                divPlayerInfo.appendChild(rmBtn);
+                let edBtn = document.createElement("button");
+                edBtn.classList.add("but");
+                edBtn.textContent = "Edit";
+                divPlayerInfo.appendChild(edBtn);
+                
                
 
                 button.addEventListener("click", () => {
@@ -135,3 +141,164 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     Exercise63();
 })
+document.addEventListener("DOMContentLoaded", () => {
+    const players = [
+        {
+            id: 10,
+            image: "bull1.webp",
+            name: "Coby White",
+            position: "Base (PG)",
+            age: 24,
+            pointsPerGame: 19.1
+        },
+        {
+            id:12,
+            image: "bull2.webp",
+            name: "Emanuel Miller",
+            position: "Alero (SF)",
+            age: 24,
+            pointsPerGame: 7.3
+        },
+        {
+            id: 31,
+            image: "bull3.webp",
+            name: "Josh Giddey",
+            position: "Escolta (SG)",
+            age: 22,
+            pointsPerGame: 12.3
+        },
+        {
+            id: 45,
+            image: "bull4.webp",
+            name: "Jevon Carter",
+            position: "Base (PG)",
+            age: 29,
+            pointsPerGame: 5.0
+        },
+        {
+            id: 50,
+            image: "bull5.webp",
+            name: "Dalen Terry",
+            position: "Escolta/Alero (SG/SF)",
+            age: 22,
+            pointsPerGame: 4.0
+        }
+    ];
+
+
+  let body = document.querySelector("body");
+
+  let container = document.createElement("div");
+  container.className = "container";
+
+  body.appendChild(container);
+
+  function cleanContainer() {
+    container.innerHTML = "";
+  }
+
+  function deletePlayer(players, id) {
+    let playerIndex = players.findIndex((player) => player.id == id);
+    players.splice(playerIndex, 1);
+  }
+
+  function drawPlayer(player) {
+    let myCard = document.querySelector("template").content.cloneNode(true);
+    let card = myCard.querySelector("div");
+    card.id = player.id;
+    let img = myCard.querySelector("img");
+    img.src = "img/" + player.image;
+    let name = myCard.querySelector("h2");
+    name.innerText = player.name;
+    let p = myCard.querySelectorAll("p");
+    let p1 = p[0];
+    let p2 = p[1];
+    let p3 = p[2];
+    p1.innerText += player.position;
+    p2.innerText += player.age + " years";
+    p3.innerText += player.pointsPerGame + " points per game";
+    // let rmBtn = myCard.querySelector(".but");
+    // let edBtn = myCard.querySelector(".edt");
+
+    container.appendChild(myCard);
+    rmBtn.addEventListener("click", () => {
+      let playerID = rmBtn.parentElement.id;
+      deletePlayer(players, playerID);
+      cleanContainer();
+      drawAllPlayers(players);
+    });
+
+    edBtn.addEventListener("click", () => {
+      let playerID = edBtn.parentElement.id;
+      let player = players.find((player) => player.id == playerID);
+      document.getElementsByName("pname")[0].value = player.name;
+      player.name = "Jose Socuellamos"
+
+
+      console.log(player);
+      
+      cleanContainer();
+      drawAllPlayers(players);
+    });
+  }
+
+  function drawAllPlayers(players) {
+    for (const player of players) {
+      drawPlayer(player);
+    }
+  }
+
+  drawAllPlayers(players);
+
+  // let myPlayerName = prompt(
+  //   "Enter the jersey number of the player you want to rename: "
+  // );
+  // let newName = prompt("Enter the new name of the player: ");
+  // let playerIndex = players.findIndex((player) => player.name == myPlayerName);
+  // players[playerIndex].name = newName;
+  // cleanContainer();
+  // drawAllPlayers(players);
+
+  let addPlayerBtn = document.querySelector("#showForm");
+
+  let formDiv = document.querySelector("#newPlayerForm");
+
+  addPlayerBtn.addEventListener("click", function () {
+    if (addPlayerBtn.textContent == "Show Form") {
+      formDiv.style.display = "block";
+      addPlayerBtn.textContent = "Hide Form";
+    } else {
+      formDiv.style.display = "none";
+      addPlayerBtn.textContent = "Show Form";
+    }
+
+    let submitBtn = document.querySelector("#submitForm");
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let idInput = document.getElementsByName("pid")[0].value;
+      let nameInput = document.getElementsByName("pname")[0].value;
+      let surnameInput = document.getElementsByName("psurname")[0].value;
+      let posInput = document.getElementsByName("position")[0].value;
+      let ageInput = document.getElementsByName("age")[0].value;
+      let ppgInput = document.getElementsByName("ppg")[0].value;
+
+      console.log(idInput);
+
+      let newPlayer = {
+        id: idInput,
+        image: "default.jpg",
+        name: nameInput + " " + surnameInput,
+        position: posInput,
+        age: ageInput,
+        pointsPerGame: ppgInput,
+      };
+
+      players.push(newPlayer);
+      document.querySelector("form").reset();
+      formDiv.style.display = "none";
+      addPlayerBtn.innerText = "Show Form";
+      cleanContainer();
+      drawAllPlayers(players);
+    });
+  });
+});
